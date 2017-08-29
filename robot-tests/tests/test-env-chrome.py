1388470f15638209b54b64b1d17b3799a11e2829
Ossi Hämäläinen <ossi.hamalainen@tieto.com>
2017-08-29 17:23:34 +0300

    Changed openshift env

--- robot-tests/tests/test-env-chrome.py
+++ robot-tests/tests/test-env-chrome.py
@@ -1,4 +1,4 @@
-URL = 'http://demoapp-test.cloudapps.ocp-tdemo4.teco.prd.a.tecdomain.net/#/'
+URL = 'http://demoapp-test.cloudapps.ocp-tdemo5.teco.prd.a.tecdomain.net/#/'
 USERNAME_A = 'admin'
 PWD_A = 'admin'
 BROWSER = 'GoogleChrome'
--- robot-tests/tests/uat-env.py
+++ robot-tests/tests/uat-env.py
@@ -1,3 +1,3 @@
-URL = 'http://app-demoapp-uat.cloudapps.ocp-tdemo4.teco.prd.a.tecdomain.net'
+URL = 'http://app-demoapp-uat.cloudapps.ocp-tdemo5.teco.prd.a.tecdomain.net'
 USERNAME_A = 'admin'
 PWD_A = 'admin'
